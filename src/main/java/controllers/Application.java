7cb1ab0de5f92785e45325179213d2775109b1f9
Богдан <bogdanlupashko@gmail.com>
2015-02-22 18:42:09 +0200

    commit

---
+++ src/main/java/controllers/Application.java
@@ -0,0 +1,10 @@
+package controllers;
+
+import javax.ws.rs.ApplicationPath;
+
+/**
+ * Created by Богдан on 22.02.2015.
+ */
+@ApplicationPath("/api")
+public class Application extends javax.ws.rs.core.Application{
+}
---
+++ src/main/resources/META-INF/load-script.sql
@@ -0,0 +1,1 @@
+---insert into users(email) values ('load.script@fucking.com')
